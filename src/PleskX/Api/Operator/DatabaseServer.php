b4a4644e4cb9d069022c6b630eba411c3c3bc16e
Alexei Yuzhakov <sibprogrammer@gmail.com>
2016-11-13 10:47:45 +0700

    Add methods to fetch database server details.

--- src/PleskX/Api/Operator/DatabaseServer.php
+++ src/PleskX/Api/Operator/DatabaseServer.php
@@ -2,6 +2,8 @@
 // Copyright 1999-2016. Parallels IP Holdings GmbH.
 
 namespace PleskX\Api\Operator;
+use PleskX\Api\Struct\DatabaseServer as Struct;
+
 
 class DatabaseServer extends \PleskX\Api\Operator
 {
@@ -15,6 +17,50 @@ class DatabaseServer extends \PleskX\Api\Operator
     {
         $response = $this->request('get-supported-types');
         return (array)$response->type;
+    }
+
+    /**
+     * @param string $field
+     * @param integer|string $value
+     * @return Struct\Info
+     */
+    public function get($field, $value)
+    {
+        $customers = $this->_get($field, $value);
+        return reset($customers);
+    }
+
+    /**
+     * @return Struct\Info[]
+     */
+    public function getAll()
+    {
+        return $this->_get();
+    }
+
+    /**
+     * @param string|null $field
+     * @param integer|string|null $value
+     * @return Struct\Info|Struct\Info[]
+     */
+    private function _get($field = null, $value = null)
+    {
+        $packet = $this->_client->getPacket();
+        $getTag = $packet->addChild('db_server')->addChild('get');
+
+        $filterTag = $getTag->addChild('filter');
+        if (!is_null($field)) {
+            $filterTag->addChild($field, $value);
+        }
+
+        $response = $this->_client->request($packet, \PleskX\Api\Client::RESPONSE_FULL);
+
+        $items = [];
+        foreach ($response->xpath('//result') as $xmlResult) {
+            $items[] = new Struct\Info($xmlResult->data);
+        }
+
+        return $items;
     }
 
 }
--- tests/DatabaseServerTest.php
+++ tests/DatabaseServerTest.php
@@ -11,4 +11,19 @@ class DatabaseServerTest extends TestCase
         $this->assertContains('mysql', $types);
     }
 
+    public function testGet()
+    {
+        $dbServer = $this->_client->databaseServer()->get('id', 1);
+        $this->assertEquals('localhost', $dbServer->host);
+        $this->assertGreaterThan(0, $dbServer->port);
+    }
+
+    public function testGetAll()
+    {
+        $dbServers = $this->_client->databaseServer()->getAll();
+        $this->assertInternalType('array', $dbServers);
+        $this->assertGreaterThan(0, count($dbServers));
+        $this->assertEquals('localhost', $dbServers[0]->host);
+    }
+
 }
