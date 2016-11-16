f4896a37a0eee896a71ccb39fc78f20837bedb7f
Alexei Yuzhakov <sibprogrammer@gmail.com>
2016-11-16 23:33:51 +0700

    Add create/delete operations for mailnames.

---
+++ tests/MailTest.php
@@ -0,0 +1,44 @@
+<?php
+// Copyright 1999-2016. Parallels IP Holdings GmbH.
+
+class MailTest extends TestCase
+{
+
+    /**
+     * @param string $name
+     * @return \PleskX\Api\Struct\Webspace\Info
+     */
+    private function _createWebspace($name)
+    {
+        return $this->_client->webspace()->create([
+            'name' => $name,
+            'ip_address' => $this->_getIpAddress(),
+        ], [
+            'ftp_login' => 'test-login',
+            'ftp_password' => 'test-password',
+        ]);
+    }
+
+    public function testCreate()
+    {
+        $webspace = $this->_createWebspace('example.dom');
+        $mailname = $this->_client->mail()->create('test', $webspace->id, true, 'secret');
+
+        $this->assertInternalType('integer', $mailname->id);
+        $this->assertGreaterThan(0, $mailname->id);
+        $this->assertEquals('test', $mailname->name);
+
+        $this->_client->webspace()->delete('id', $webspace->id);
+    }
+
+    public function testDelete()
+    {
+        $webspace = $this->_createWebspace('example.dom');
+        $mailname = $this->_client->mail()->create('test', $webspace->id);
+
+        $result = $this->_client->mail()->delete('name', $mailname->name, $webspace->id);
+        $this->assertTrue($result);
+        $this->_client->webspace()->delete('id', $webspace->id);
+    }
+
+}
