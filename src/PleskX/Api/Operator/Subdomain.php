a624dcce8b93a1ed251100092eadf7cc8e81ea54
Vladislav Baranovskiy <vbaranovskiy@parallels.com>
2016-11-21 15:50:00 +0700

    Add get and getAll operations for subdomains

--- src/PleskX/Api/Operator/Subdomain.php
+++ src/PleskX/Api/Operator/Subdomain.php
@@ -32,4 +32,42 @@ class Subdomain extends \PleskX\Api\Operator
     {
         return $this->_delete($field, $value);
     }
+
+    /**
+     * @param string $field
+     * @param integer|string $value
+     * @return Struct\Info
+     */
+    public function get($field, $value)
+    {
+        $items = $this->getAll($field, $value);
+        return reset($items);
+    }
+
+    /**
+     * @param string $field
+     * @param integer|string $value
+     * @return Struct\Info[]
+     */
+    public function getAll($field = null, $value = null)
+    {
+        $packet = $this->_client->getPacket();
+        $getTag = $packet->addChild($this->_wrapperTag)->addChild('get');
+
+        $filterTag = $getTag->addChild('filter');
+        if (!is_null($field)) {
+            $filterTag->addChild($field, $value);
+        }
+
+        $response = $this->_client->request($packet, \PleskX\Api\Client::RESPONSE_FULL);
+
+        $items = [];
+        foreach ($response->xpath('//result') as $xmlResult) {
+            $item = new Struct\Info($xmlResult->data);
+            $item->id = (int)$xmlResult->id;
+            $items[] = $item;
+        }
+
+        return $items;
+    }
 }
