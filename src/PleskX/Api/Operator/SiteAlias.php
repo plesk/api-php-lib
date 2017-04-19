62b9f5eb38d71e2d61117f7492fd9a80f2ea5990
dpfaffenbauer <dominik@lineofcode.at>
2017-04-19 17:52:30 +0200

    add additional preferences for site-alias creation

--- src/PleskX/Api/Operator/SiteAlias.php
+++ src/PleskX/Api/Operator/SiteAlias.php
@@ -6,14 +6,23 @@ use PleskX\Api\Struct\SiteAlias as Struct;
 
 class SiteAlias extends \PleskX\Api\Operator
 {
-/**
+    /**
      * @param array $properties
+     * @param array $preferences
      * @return Struct\Info
      */
-    public function create(array $properties)
+    public function create(array $properties, array $preferences = [])
     {
         $packet = $this->_client->getPacket();
         $info = $packet->addChild($this->_wrapperTag)->addChild('create');
+
+        if (count($preferences) > 0) {
+            $prefs = $info->addChild('pref');
+
+            foreach ($preferences as $key => $value) {
+                $prefs->addChild($key, is_bool($value) ? ($value ? 1 : 0) : $value);
+            }
+        }
 
         $info->addChild('site-id', $properties['site-id']);
         $info->addChild('name', $properties['name']);
