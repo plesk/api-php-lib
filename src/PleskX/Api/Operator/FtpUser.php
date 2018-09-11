dd2b46178b28abec8d1210d6d9c8cb84a021b99f
armando <armando@shellrent.com>
2018-09-11 17:20:57 +0200

    aggiunto chiamata di get per gli utenti ftp

--- src/PleskX/Api/Operator/FtpUser.php
+++ src/PleskX/Api/Operator/FtpUser.php
@@ -2,10 +2,30 @@
 
 namespace PleskX\Api\Operator;
 
-use PleskX\Api\Struct\FtpUser\Info as Struct;
+use PleskX\Api\Struct\FtpUser\Info;
 
 
 class FtpUser extends \PleskX\Api\Operator {
+	
+    /**
+     * @param $command
+     * @param $field
+     * @param $value
+     * @return \PleskX\Api\XmlResponse
+     */
+    private function _get($command, $field, $value) {
+        $packet = $this->_client->getPacket();
+        $getTag = $packet->addChild($this->_wrapperTag)->addChild($command);
+
+        $filterTag = $getTag->addChild('filter');
+        if (!is_null($field)) {
+            $filterTag->addChild($field, $value);
+        }
+
+        $response = $this->_client->request($packet, \PleskX\Api\Client::RESPONSE_FULL);
+        return $response;
+    }
+	
 	
 	/**
 	 * @param string $ftpUser
@@ -21,9 +41,24 @@ class FtpUser extends \PleskX\Api\Operator {
 		
 		$values = $set->addChild( 'values' );
 		$values->addChild( 'password', $newPassword );
-
+		
 		$response = $this->_client->request($packet);
 		
-		return new Struct( $response );
+		return new Info( $response );
 	}
+	
+	
+	 /**
+     * @param string $field
+     * @param integer|string $value
+     * @return Struct\Info[]
+     */
+    public function getAll($field = null, $value = null) {
+        $response = $this->_get('get', $field, $value);
+        $items = [];
+        foreach ($response->xpath('//result') as $xmlResult) {
+            $items[] = new Struct\Info($xmlResult);
+        }
+        return $items;
+    }
 }
--- src/PleskX/Api/Struct/FtpUser/Info.php
+++ src/PleskX/Api/Struct/FtpUser/Info.php
@@ -6,29 +6,38 @@ class Info extends \PleskX\Api\Struct
 {
 
     /**
-	 * Stato della richiesta
-	 * @var string
-	 */
-    public $status;
-
-    /**
-	 * Nome utenza ftp
-	 * @var string
-	 */
-    public $filterId;
-
-    /**
-	 * Identificatore univoco dell'account ftp
 	 * @var int
 	 */
     public $id;
 
+    /**
+	 * @var int
+	 */
+    public $name;
+
+    /**
+	 * @var int
+	 */
+    public $home;
+
+    /**
+	 * @var int
+	 */
+    public $quota;
+
+    /**
+	 * @var int
+	 */
+    public $webspaceId;
+	
 	
     public function __construct( $apiResponse ) {
         $this->_initScalarProperties($apiResponse, [
-            'status',
-            'filter-id',
             'id',
+            'name',
+            'home',
+            'quota',
+            'webspace_id',
         ]);
     }
 }
