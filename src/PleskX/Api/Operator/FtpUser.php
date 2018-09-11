70ef8b4b3344d74ed38b09ebf62cdbdab3f5b87d
armando <armando@shellrent.com>
2018-09-11 12:37:52 +0200

    aggiunta chiamata per l'aggiornamento della password ftp

--- src/PleskX/Api/Operator/FtpUser.php
+++ src/PleskX/Api/Operator/FtpUser.php
@@ -1,9 +1,29 @@
 <?php
-// Copyright 1999-2016. Parallels IP Holdings GmbH.
 
 namespace PleskX\Api\Operator;
 
-class FtpUser extends \PleskX\Api\Operator
-{
+use PleskX\Api\Struct\FtpUser\Info as Struct;
 
+
+class FtpUser extends \PleskX\Api\Operator {
+	
+	/**
+	 * @param string $ftpUser
+	 * @param string $newPassword
+	 * @return \PleskX\Api\Struct\FtpUser\Info
+	 */
+	public function updateFtpPassword( $ftpUser, $newPassword ) {
+		$packet = $this->_client->getPacket();
+		$set = $packet->addChild( $this->_wrapperTag )->addChild( 'set' );
+		
+		$filter = $set->addChild( 'filter' );
+		$filter->addChild( 'name', $ftpUser );
+		
+		$values = $set->addChild( 'values' );
+		$values->addChild( 'password', $newPassword );
+
+		$response = $this->_client->request($packet);
+		
+		return new Struct( $response );
+	}
 }
