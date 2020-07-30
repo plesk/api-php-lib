3428abccacf3c895eb52fc235b971336b1be7868
Alexei Yuzhakov <ayuzhakov@plesk.com>
2020-07-30 21:23:08 +0700

    Fix redirect handling in case of latest Plesk

--- wait-for-plesk.sh
+++ wait-for-plesk.sh
@@ -2,7 +2,7 @@
 ### Copyright 1999-2020. Plesk International GmbH.
 
 while : ; do
-    curl -ks https://plesk:8443/ | grep "<title>Plesk" > /dev/null
+    curl -ksL https://plesk:8443/ | grep "<title>Plesk" > /dev/null
     [ $? -eq 0 ] && break
     sleep 5
 done
