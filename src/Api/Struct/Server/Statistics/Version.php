92a2b6e4301e79078b86259cec1d627f689f2674
rene-gates <development@lazybox.de>
2021-01-31 01:03:17 +0100

    Make additional parameters available as property of class

--- src/Api/Struct/Server/Statistics/Version.php
+++ src/Api/Struct/Server/Statistics/Version.php
@@ -11,11 +11,27 @@ class Version extends \PleskX\Api\Struct
     /** @var string */
     public $version;
 
+    /** @var string */
+    public $build;
+
+    /** @var string */
+    public $osName;
+
+    /** @var string */
+    public $osVersion;
+
+    /** @var string */
+    public $osRelease;
+
     public function __construct($apiResponse)
     {
         $this->_initScalarProperties($apiResponse, [
             ['plesk_name' => 'internalName'],
             ['plesk_version' => 'version'],
+            ['plesk_build' => 'build'],
+            ['plesk_os' => 'osName'],
+            ['plesk_os_version' => 'osVersion'],
+            ['os_release' => 'osRelease'],
         ]);
     }
 }
