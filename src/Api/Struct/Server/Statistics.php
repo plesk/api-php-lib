c122739cc71d97a123d24f33fc15451b0c118277
rene-gates <development@lazybox.de>
2021-02-11 23:28:53 +0100

    Update Statistics.php
    Add missing objects in statistic response

--- src/Api/Struct/Server/Statistics.php
+++ src/Api/Struct/Server/Statistics.php
@@ -11,9 +11,33 @@ class Statistics extends \PleskX\Api\Struct
     /** @var Statistics\Version */
     public $version;
 
+    /** @var Statistics\Other */
+    public $other;
+
+    /** @var Statistics\LoadAvg */
+    public $loadAvg;
+
+    /** @var Statistics\Memory */
+    public $memory;
+
+    /** @var Statistics\Swap */
+    public $swap;
+
+    /** @var array */
+    public $diskSpace;
+
     public function __construct($apiResponse)
     {
         $this->objects = new Statistics\Objects($apiResponse->objects);
         $this->version = new Statistics\Version($apiResponse->version);
+        $this->other = new Statistics\Other($apiResponse->other);
+        $this->loadAvg = new Statistics\LoadAvg($apiResponse->load_avg);
+        $this->memory = new Statistics\Memory($apiResponse->mem);
+        $this->swap = new Statistics\Swap($apiResponse->swap);
+
+        $this->diskSpace = [];
+        foreach ($apiResponse->diskspace as $disk) {
+            $this->diskSpace[(string) $disk->device->name] = new Statistics\DiskSpace($disk->device);
+        }
     }
 }
