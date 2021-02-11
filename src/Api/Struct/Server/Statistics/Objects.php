055d649fd3aad8204e73a06229722cdcb42aa7ea
rene-gates <development@lazybox.de>
2021-02-11 23:30:10 +0100

    Update Objects.php
    Add missing fields to Statistic Objects

--- src/Api/Struct/Server/Statistics/Objects.php
+++ src/Api/Struct/Server/Statistics/Objects.php
@@ -11,11 +11,51 @@ class Objects extends \PleskX\Api\Struct
     /** @var int */
     public $domains;
 
+    /** @var int */
+    public $activeDomains;
+
+    /** @var int */
+    public $mailBoxes;
+
+    /** @var int */
+    public $mailRedirects;
+
+    /** @var int */
+    public $mailGroups;
+
+    /** @var int */
+    public $mailResponders;
+
+    /** @var int */
+    public $webUsers;
+
+    /** @var int */
+    public $databases;
+
+    /** @var int */
+    public $databaseUsers;
+
+    /** @var int */
+    public $problemClients;
+
+    /** @var int */
+    public $problemDomains;
+
     public function __construct($apiResponse)
     {
         $this->_initScalarProperties($apiResponse, [
-            'clients',
-            'domains',
+            ['clients' => 'clients'],
+            ['domains' => 'domains'],
+            ['active_domains' => 'activeDomains'],
+            ['mail_boxes' => 'mailBoxes'],
+            ['mail_redirects' => 'mailRedirects'],
+            ['mail_groups' => 'mailGroups'],
+            ['mail_responders' => 'mailResponders'],
+            ['web_users' => 'webUsers'],
+            ['databases' => 'databases'],
+            ['database_users' => 'databaseUsers'],
+            ['problem_clients' => 'problemClients'],
+            ['problem_domains' => 'problemDomains'],
         ]);
     }
 }
