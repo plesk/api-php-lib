0f3a573e96cde8e2e753acd971e7775ea1c8b803
Michael Münch <michael.muench@itanix.de>
2021-03-25 09:03:14 +0100

    Add additional fields to Struct Webspace\GeneralInfo
    Add ipAddresses to Struct Webspace\GeneralInfo
    
    Add ipAddresses to Struct Webspace\GeneralInfo
    
    Add additional fields to struct Webspace/GeneralInfo

--- src/Api/Struct/Webspace/GeneralInfo.php
+++ src/Api/Struct/Webspace/GeneralInfo.php
@@ -6,20 +6,56 @@ namespace PleskX\Api\Struct\Webspace;
 class GeneralInfo extends \PleskX\Api\Struct
 {
     /** @var string */
+    public $crDate;
+
+    /** @var string */
     public $name;
 
     /** @var string */
-    public $guid;
+    public $asciiName;
+
+    /** @var string */
+    public $status;
 
     /** @var int */
     public $realSize;
 
+    /** @var int */
+    public $ownerId;
+
+    /** @var array */
+    public $ipAddresses = [];
+
+    /** @var string */
+    public $guid;
+
+    /** @var string */
+    public $vendorGuid;
+
+    /** @var string */
+    public $description;
+
+    /** @var string */
+    public $adminDescription;
+
     public function __construct($apiResponse)
     {
         $this->_initScalarProperties($apiResponse, [
+            'cr_date',
             'name',
-            'guid',
+            'ascii-name',
+            'status',
             'real_size',
+            'owner-id',
+            'guid',
+            'vendor-guid',
+            'description',
+            'admin-description'
         ]);
+
+        foreach($apiResponse->dns_ip_address as $ip)
+        {
+            $this->ipAddresses[] = (string)$ip;
+        }
     }
 }
