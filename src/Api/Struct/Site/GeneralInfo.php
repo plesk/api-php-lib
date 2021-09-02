9d36db668bbc9074e71d35668f9930ec3179f825
Simon Solutions <development@lazybox.de>
2021-09-02 10:08:51 +0200

    Add additional fields for site

--- src/Api/Struct/Site/GeneralInfo.php
+++ src/Api/Struct/Site/GeneralInfo.php
@@ -6,6 +6,9 @@ namespace PleskX\Api\Struct\Site;
 class GeneralInfo extends \PleskX\Api\Struct
 {
     /** @var string */
+    public $crDate;
+
+    /** @var string */
     public $name;
 
     /** @var string */
@@ -17,17 +20,37 @@ class GeneralInfo extends \PleskX\Api\Struct
     /** @var string */
     public $status;
 
+    /** @var int */
+    public $realSize;
+
+    /** @var array */
+    public $ipAddresses = [];
+
     /** @var string */
     public $description;
+
+    /** @var string */
+    public $webspaceGuid;
+
+    /** @var int */
+    public $webspaceId;
 
     public function __construct($apiResponse)
     {
         $this->_initScalarProperties($apiResponse, [
+            'cr_date',
             'name',
             'ascii-name',
             'status',
+            'real_size',
             'guid',
             'description',
+            'webspace-guid',
+            'webspace-id',
         ]);
+
+        foreach ($apiResponse->dns_ip_address as $ip) {
+            $this->ipAddresses[] = (string) $ip;
+        }
     }
 }
