796ea2c8eeead4e6c17793fcef60c2ba56d74c05
Alexandr Bashurov <abashurov@plesk.com>
2022-03-28 20:25:14 +0700

    BUGFIX EXTREST-143 Extend already set properties instead of overwriting them

--- src/Api/Client.php
+++ src/Api/Client.php
@@ -349,8 +349,10 @@ class Client
             $el = is_int($key) && $parentEl ? $parentEl : $key;
             if (is_array($value)) {
                 $this->_arrayToXml($value, $this->_isAssocArray($value) ? $xml->addChild($el) : $xml, $el);
-            } else {
+            } elseif(!isset($xml->{$el})) {
                 $xml->{$el} = (string) $value;
+            } else {
+                $xml->{$el}[] = (string) $value;
             }
         }
 
