99e8b979c601189653eade1358996ef47698fe0e
Alexei Yuzhakov <ayuzhakov@plesk.com>
2023-03-22 16:53:14 +0200

    Implement SSL certificates install/remove procedures
    Co-authored-by: Bobi <69676022+bobicloudvision@users.noreply.github.com>

--- src/Api/Operator/Certificate.php
+++ src/Api/Operator/Certificate.php
@@ -20,4 +20,38 @@ class Certificate extends \PleskX\Api\Operator
 
         return new Struct\Info($response);
     }
+
+    public function install(array $properties, string $request, string $privateKey): bool
+    {
+        $packet = $this->client->getPacket();
+
+        $installTag = $packet->addChild($this->wrapperTag)->addChild('install');
+        foreach ($properties as $name => $value) {
+            $installTag->{$name} = $value;
+        }
+
+        $contentTag = $installTag->addChild('content');
+        $contentTag->addChild('csr', $request);
+        $contentTag->addChild('pvt', $privateKey);
+
+        $result = $this->client->request($packet);
+
+        return 'ok' == (string) $result->status;
+    }
+
+    public function delete(string $name, array $properties): bool
+    {
+        $packet = $this->client->getPacket();
+
+        $removeTag = $packet->addChild($this->wrapperTag)->addChild('remove');
+        $removeTag->addChild('filter')->addChild('name', $name);
+
+        foreach ($properties as $name => $value) {
+            $removeTag->{$name} = $value;
+        }
+
+        $result = $this->client->request($packet);
+
+        return 'ok' == (string) $result->status;
+    }
 }
--- tests/CertificateTest.php
+++ tests/CertificateTest.php
@@ -5,20 +5,48 @@ namespace PleskXTest;
 
 class CertificateTest extends AbstractTestCase
 {
+    private array $certificateProperties = [
+        'bits' => 2048,
+        'country' => 'CH',
+        'state' => 'Schaffhausen',
+        'location' => 'Schaffhausen',
+        'company' => 'Plesk',
+        'email' => 'info@plesk.com',
+        'name' => 'plesk.com',
+    ];
+
     public function testGenerate()
     {
-        $certificate = static::$client->certificate()->generate([
-            'bits' => 2048,
-            'country' => 'RU',
-            'state' => 'NSO',
-            'location' => 'Novosibirsk',
-            'company' => 'Plesk',
-            'email' => 'info@plesk.com',
-            'name' => 'plesk.com',
-        ]);
+        $certificate = static::$client->certificate()->generate($this->certificateProperties);
         $this->assertGreaterThan(0, strlen($certificate->request));
         $this->assertStringStartsWith('-----BEGIN CERTIFICATE REQUEST-----', $certificate->request);
         $this->assertGreaterThan(0, strlen($certificate->privateKey));
         $this->assertStringStartsWith('-----BEGIN PRIVATE KEY-----', $certificate->privateKey);
+    }
+
+    public function testInstall()
+    {
+        $certificate = static::$client->certificate()->generate($this->certificateProperties);
+
+        $result = static::$client->certificate()->install([
+            'name' => 'test',
+            'admin' => true,
+        ], $certificate->request, $certificate->privateKey);
+        $this->assertTrue($result);
+
+        static::$client->certificate()->delete('test', ['admin' => true]);
+    }
+
+    public function testDelete()
+    {
+        $certificate = static::$client->certificate()->generate($this->certificateProperties);
+
+        static::$client->certificate()->install([
+            'name' => 'test',
+            'admin' => true,
+        ], $certificate->request, $certificate->privateKey);
+
+        $result = static::$client->certificate()->delete('test', ['admin' => true]);
+        $this->assertTrue($result);
     }
 }
