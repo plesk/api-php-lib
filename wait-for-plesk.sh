f04133177f63231211935933b73cfed6c8ea7fd2
Alexei Yuzhakov <alexei.yuzhakov@webpros.com>
2024-07-08 16:35:33 +0100

    TECH Introduce 5 minutes timeout for the Plesk initialization

--- wait-for-plesk.sh
+++ wait-for-plesk.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 ### Copyright 1999-2024. WebPros International GmbH.
 
+COUNTER=1
+
 while : ; do
     curl -ksL https://plesk:8443/ | grep "<title>Plesk" > /dev/null
     [ $? -eq 0 ] && break
+    echo "($COUNTER) Waiting for the Plesk initialization..."
     sleep 5
+    COUNTER=$((COUNTER + 1))
+    if [ $COUNTER -eq 60 ]; then
+        echo "Too long, interrupting..."
+        break
+    fi
 done
